1c30b392e9e0533b629bbde15dfe4d93f2a674b8
nirperetz <nir_@gmail.com>
2014-09-09 15:22:23 +0300

    del

--- js/jsonp.js
+++
@@ -1,1 +0,0 @@
-cdx.f({'a':'aqfQCW4dayG2aaaalualvjTbnrWaCJeHnqTuac9ac1taDETzaaaaaaaaaaaaaaaababIagCHaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1@Kaaaaaaaaa','b':'OK','c':1,'d':13960, 'e':'IL'});
